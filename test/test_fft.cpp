982b5ecf5914c0d3878de8dc3c4669d535246750
Yuxuan Ye <littleye233@gmail.com>
2026-07-19 13:07:22 +0800

    feat(test): add test_implot and set up clang tools
    Selects GLFW + OpenGL technical stack. OpenGL should be provided by
    GPU drivers and GLFW is statically linked.
    
    Provides some CMake configurable options to tweak final artifacts.

--- test/test_fft.cpp
+++ test/test_fft.cpp
@@ -1,7 +1,7 @@
 #define _USE_MATH_DEFINES
 
-#include <complex>
 #include <cmath>
+#include <complex>
 #include <cstddef>
 #include <iostream>
 #include <pocketfft_hdronly.h>
@@ -11,35 +11,29 @@ using namespace std;
 
 int main() {
     // Sampling frequency = 8000 Hz
-    const double fs = 8000;
+    const double FS = 8000;
     // Signal frequency = 1000 Hz
-    const double f_sig = 1000;
+    const double F_SIG = 1000;
     // Number of samples
     const size_t N = 8;
 
     // Signal: x[n]=3*sin(2*pi*f_sig*n/fs)
     vector<double> data_in(N);
     for (size_t n = 0; n < N; ++n) {
-        data_in[n] = 3 * sin(2.0 * M_PI * f_sig * static_cast<double>(n) / fs);
+        data_in[n] = 3 * sin(2.0 * M_PI * F_SIG * static_cast<double>(n) / FS);
     }
 
     size_t output_size = N / 2 + 1;
     vector<complex<double>> data_out(output_size);
 
     // Set up FFT axis configurations
-    pocketfft::shape_t shape_in = { N };
-    pocketfft::stride_t stride_in = { sizeof(double) };
-    pocketfft::stride_t stride_out = { sizeof(complex<double>) };
+    pocketfft::shape_t shape_in = {N};
+    pocketfft::stride_t stride_in = {sizeof(double)};
+    pocketfft::stride_t stride_out = {sizeof(complex<double>)};
 
-    pocketfft::r2c(
-        shape_in,
-        stride_in,
-        stride_out,
-        (size_t)0,
-        pocketfft::FORWARD,
-        data_in.data(),
-        data_out.data(),
-        2.0 / N // normalization factor
+    pocketfft::r2c(shape_in, stride_in, stride_out, (size_t)0,
+                   pocketfft::FORWARD, data_in.data(), data_out.data(),
+                   2.0 / N // normalization factor
     );
 
     // Output
---
+++ test/test_implot.cpp
@@ -0,0 +1,123 @@
+// MIT License
+
+// Copyright (c) 2020-2024 Evan Pezent
+// Copyright (c) 2025-2026 Breno Cunha Queiroz
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "implot.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <ostream>
+
+// Callback to handle GLFW errors
+static void glfwErrorCallback(int error, const char *description) {
+    std::cerr << "GLFW Error " << error << ": " << description << std::endl;
+}
+
+int main() {
+    // Setup error callback
+    glfwSetErrorCallback(glfwErrorCallback);
+
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Setup OpenGL version
+    // GL 3.0 + GLSL 130 (Windows and Linux)
+    const char *glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    // Setup DPI scaling
+    /// @note Only set up window. Still configure ImGUI.
+    glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
+
+    // Create window
+    GLFWwindow *window =
+        glfwCreateWindow(1200, 800, "ImPlot Example", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(0); // Disable vsync
+
+    float xscale, yscale;
+    glfwGetWindowContentScale(window, &xscale, &yscale);
+    std::cout << xscale << ' ' << yscale << std::endl;
+
+    // Setup context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImPlot::CreateContext();
+
+    // Setup style
+    ImGui::StyleColorsDark();
+
+    // Setup font
+    ImGui::GetStyle().ScaleAllSizes(xscale);
+    ImGui::GetIO().Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\msyhl.ttc");
+
+    // Setup backend
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        // Start frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Demo windows
+        ImGui::ShowDemoWindow();
+        ImPlot::ShowDemoWindow();
+
+        // Render
+        ImGui::Render();
+        int display_w, display_h;
+        /// @note This should handle scaling well.
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImPlot::DestroyContext();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
